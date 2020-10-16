03730b888b96a2b9c4bed41c1b815385a1d27a45
Kuznetsov Dmitry <rulff@yandex.ru>
2020-10-16 23:52:00 +0300

    Initial Commit

---
+++ ChangeColorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeColorApp
+//
+//  Created by Дмитрий Кузнецов on 16/10/2020.
+//  Copyright © 2020 Дмитрий Кузнецов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
